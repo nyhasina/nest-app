fbac2103fce1cf751e1b044e0241a4ae2a66f22f
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-09 01:12:58 +0300

    Remove unused members

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/user/user.model';
-import { PrimaryGeneratedColumn, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  @PrimaryGeneratedColumn()
-  id: number;
-
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
