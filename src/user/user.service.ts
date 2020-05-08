0007f3cc9444a1ee90ba5f63d1a08a35df8f0c45
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-09 00:38:46 +0300

    Register user repository

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.model';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+  }
+}
