e2452e529d2e0ff26a95931121dc58e1019325b4
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 21:56:32 +0300

    Delete authentication controller

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from 'src/authentication/local.strategy';
 import { UserModule } from 'src/user/user.module';
-import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
 
 @Module({
   imports: [UserModule, PassportModule],
   providers: [AuthenticationService, LocalStrategy],
-  controllers: [AuthenticationController],
 })
 export class AuthenticationModule {
 }
