a53a24fac0c4000c79f4d57fff1171a2a960e542
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 21:58:42 +0300

    Remove default endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,5 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
-
-  @UseGuards(AuthGuard('local'))
-  @Post('login')
-  async login(@Request() req) {
-    return req.user;
-  }
 }
