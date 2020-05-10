59536ab0f059b2dab676d53c53d13077b06b20cb
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 21:59:21 +0300

    Move login handler into a separate agnostic folder

---
+++ src/authentication/authentication.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthenticationController {
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Request() req) {
+    return req.user;
+  }
+}
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -3,10 +3,12 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from 'src/authentication/local.strategy';
 import { UserModule } from 'src/user/user.module';
 import { AuthenticationService } from './authentication.service';
+import { AuthenticationController } from './authentication.controller';
 
 @Module({
   imports: [UserModule, PassportModule],
   providers: [AuthenticationService, LocalStrategy],
+  controllers: [AuthenticationController],
 })
 export class AuthenticationModule {
 }
