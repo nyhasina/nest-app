413217e8306d63b15d4d4c5e35f6224e6c1bc0aa
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 20:28:52 +0300

    Handle null and undefined value

--- src/response.interceptor.ts
+++ src/response.interceptor.ts
@@ -6,6 +6,6 @@ import { ApiResponse } from 'src/api-response.interface';
 @Injectable()
 export class ResponseInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
   intercept(context: ExecutionContext, next: CallHandler<T>): Observable<ApiResponse<T>> | Promise<Observable<ApiResponse<T>>> {
-    return next.handle().pipe(map(data => ({ statusCode: HttpStatus.OK, data })));
+    return next.handle().pipe(map(data => ({ statusCode: HttpStatus.OK, data: data || null })));
   }
 }
