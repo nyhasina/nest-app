53ba46923588281e998f14f4cb0b483dc6ca5cc6
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-11 00:15:11 +0300

    Add user profile route

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthenticationService } from 'src/authentication/authentication.service';
+import { JwtAuthGuard } from 'src/authentication/guards/jwt-auth.guard';
+import { LocalAuthGuard } from 'src/authentication/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private authService: AuthenticationService) {
   }
 
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async me(@Request() req) {
+    return req.user;
   }
 }
