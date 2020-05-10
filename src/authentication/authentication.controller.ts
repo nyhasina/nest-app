76b35eb60adc864a880ac3ccedff97c6b1865b97
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-11 00:32:25 +0300

    Register class serializer

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthenticationService } from 'src/authentication/authentication.service';
 import { JwtAuthGuard } from 'src/authentication/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/authentication/guards/local-auth.guard';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
 export class AuthenticationController {
   constructor(private authService: AuthenticationService) {
   }
