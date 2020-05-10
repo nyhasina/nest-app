86c5a7bd6894158631a79ab93ed44410fec26ccf
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 23:05:29 +0300

    Configure jwt module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { jwtConstants } from 'src/authentication/authentication.constants';
 import { LocalStrategy } from 'src/authentication/local.strategy';
 import { UserModule } from 'src/user/user.module';
-import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { AuthenticationService } from './authentication.service';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthenticationService, LocalStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secretKey,
+      signOptions: {
+        expiresIn: '3600s',
+      },
+    }),
+  ],
+  providers: [LocalStrategy, AuthenticationService],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {
