e24e2b4e72ba42ecba508259df1411634d3de387
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 21:21:49 +0300

    Edit validate's signature

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { User } from 'src/user/user.model';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
@@ -8,12 +7,12 @@ export class AuthenticationService {
   constructor(private userService: UserService) {
   }
 
-  async validateUser(payload: Partial<User>): Promise<any> {
-    const user = await this.userService.findOne({ email: payload.email });
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findOne({ email });
     if (!user) {
       throw new UnauthorizedException();
     }
-    const isValid = await bcrypt.compare(payload.password, user.password);
+    const isValid = await bcrypt.compare(password, user.password);
     if (!isValid) {
       return null;
     }
--- src/authentication/local.strategy.ts
+++ src/authentication/local.strategy.ts
@@ -1,14 +1,13 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthenticationService } from 'src/authentication/authentication.service';
-import { User } from 'src/user/user.model';
 
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authenticationService: AuthenticationService) {
     super();
   }
 
-  validate(payload: Partial<User>): Promise<any> {
-    return this.authenticationService.validateUser(payload);
+  validate(email: string, password: string): Promise<any> {
+    return this.authenticationService.validateUser(email, password);
   }
 }
