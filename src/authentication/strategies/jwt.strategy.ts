64a586118611d2497adb9529fc34563669aa92b4
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-11 00:15:27 +0300

    Create jwt strategy

---
+++ src/authentication/strategies/jwt.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from 'src/authentication/authentication.constants';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private userService: UserService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secretKey,
+    });
+  }
+
+  async validate(payload: any) {
+    return this.userService.findOne({ id: payload.sub });
+  }
+}
