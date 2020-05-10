23e19ec2fd5838ae967b512502191617e936af19
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 17:06:31 +0300

    Refactor user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,35 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { BaseService } from 'src/shared/base.service';
 import { User } from 'src/user/user.model';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {
+export class UserService extends BaseService<User> {
   constructor(
     @InjectRepository(User)
-    private userRepository: Repository<User>,
+      userRepository: Repository<User>,
   ) {
-  }
-
-  create(user: User): Promise<User> {
-    return this.userRepository.save(user);
-  }
-
-  find(): Promise<User[]> {
-    return this.userRepository.find();
-  }
-
-  findOne(id: number): Promise<User> {
-    return this.userRepository.findOne(id);
-  }
-
-  async update(id: number, user: User): Promise<User> {
-    await this.userRepository.update(id, user);
-    return user;
-  }
-
-  async delete(id): Promise<boolean> {
-    await this.userRepository.delete(id);
-    return true;
+    super(userRepository);
   }
 }
