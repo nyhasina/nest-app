dd092c182bce30622f1b56d37332436657f36ec9
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 23:06:00 +0300

    Implement jwt token generation

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthenticationService } from 'src/authentication/authentication.service';
 
 @Controller('auth')
 export class AuthenticationController {
+  constructor(private authService: AuthenticationService) {
+  }
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
