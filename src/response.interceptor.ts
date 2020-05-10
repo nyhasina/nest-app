536c3c392ad07add06347f2be9763f15750d1937
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 18:13:12 +0300

    Create response interceptor

---
+++ src/response.interceptor.ts
@@ -0,0 +1,11 @@
+import { CallHandler, ExecutionContext, HttpStatus, Injectable, NestInterceptor } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ApiResponse } from 'src/api-response.interface';
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<T, ApiResponse<T>> {
+  intercept(context: ExecutionContext, next: CallHandler<T>): Observable<ApiResponse<T>> | Promise<Observable<ApiResponse<T>>> {
+    return next.handle().pipe(map(data => ({ statusCode: HttpStatus.OK, data })));
+  }
+}
