2cf5f5ff9f05d592d560e4a01543f70efeb3bc13
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 23:04:45 +0300

    Edit user id type

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -3,14 +3,14 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: number;
 
   @Column()
   username: string;
 
   @Column({
-    nullable: true
+    nullable: true,
   })
   email: string;
 
