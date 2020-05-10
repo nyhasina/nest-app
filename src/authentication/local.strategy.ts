3a86b34e0d43b8e7b8943601953333612a2f4b79
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 21:54:05 +0300

    Add injectable decorator and edit validate user signature

--- src/authentication/local.strategy.ts
+++ src/authentication/local.strategy.ts
@@ -1,13 +1,15 @@
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthenticationService } from 'src/authentication/authentication.service';
 
+@Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authenticationService: AuthenticationService) {
     super();
   }
 
-  validate(email: string, password: string): Promise<any> {
-    return this.authenticationService.validateUser(email, password);
+  validate(username: string, password: string): Promise<any> {
+    return this.authenticationService.validateUser(username, password);
   }
 }
