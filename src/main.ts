9926971b63609f2a0da1ce24587897d8750bad0e
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 18:14:23 +0300

    Register response interceptor globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ResponseInterceptor } from 'src/response.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
+
 bootstrap();
