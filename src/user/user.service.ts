024665205c79817fcea6566af06682eb28dd6e46
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-10 20:24:14 +0300

    Override base service's create method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { BaseService } from 'src/shared/base.service';
 import { User } from 'src/user/user.model';
 import { Repository } from 'typeorm';
@@ -9,7 +11,15 @@ export class UserService extends BaseService<User> {
   constructor(
     @InjectRepository(User)
       userRepository: Repository<User>,
+    private configService: ConfigService,
   ) {
     super(userRepository);
+  }
+
+  async create(payload: Partial<User>): Promise<User> {
+    const saltRound = +this.configService.get<number>('SALT_ROUND');
+    const password = await bcrypt.hash(payload.password, saltRound);
+    const user = new User({ ...payload, password });
+    return this.repository.save(user);
   }
 }
