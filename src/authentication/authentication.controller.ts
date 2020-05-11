4059f3b6f70b170c18b9e58bc3f061401f395b24
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-11 16:31:06 +0300

    Add register endpoint

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -1,4 +1,5 @@
 import {
+  Body,
   ClassSerializerInterceptor,
   Controller,
   Get,
@@ -12,23 +13,35 @@ import {
 import { AuthenticationService } from 'src/authentication/authentication.service';
 import { JwtAuthGuard } from 'src/authentication/guards/jwt-auth.guard';
 import { LocalAuthGuard } from 'src/authentication/guards/local-auth.guard';
+import { User } from 'src/user/user.model';
+import { UserService } from 'src/user/user.service';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
 export class AuthenticationController {
-  constructor(private authService: AuthenticationService) {
+  constructor(
+    private authService: AuthenticationService,
+    private userService: UserService,
+  ) {
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(HttpStatus.OK)
-  async login(@Request() req) {
+  async login(@Request() req): Promise<object> {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('register')
+  @HttpCode(HttpStatus.OK)
+  async register(@Body() payload: Partial<User>): Promise<User> {
+    return this.userService.create(payload);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get('me')
-  async me(@Request() req) {
+  async me(@Request() req): Promise<User> {
     return req.user;
   }
 }
