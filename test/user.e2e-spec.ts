07d2615dfc3a6e2fe9b68827304ca29f425d4141
Ny Hasinavalona Randriantsarafara <hrandriantsarafara@bocasay.com>
2020-05-11 19:17:02 +0300

    Create user e2e testing

---
+++ test/user.e2e-spec.ts
@@ -0,0 +1,48 @@
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as request from 'supertest';
+import { AuthenticationModule } from '../src/authentication/authentication.module';
+import { JwtAuthGuard } from '../src/authentication/guards/jwt-auth.guard';
+import { CoreModule } from '../src/core/core.module';
+import { SharedModule } from '../src/shared/shared.module';
+import { UserModule } from '../src/user/user.module';
+import { UserService } from '../src/user/user.service';
+
+export class JwtMockGuard {
+}
+
+describe('User', () => {
+  let app: INestApplication;
+  const userService = {
+    find: () => [{
+      id: 1,
+      username: 'john.doe',
+      email: 'john.doe@test.com',
+      password: 'password',
+    }],
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CoreModule, SharedModule, UserModule, AuthenticationModule],
+    })
+                                .overrideProvider(UserService)
+                                .useValue(userService)
+                                .overrideGuard(JwtAuthGuard)
+                                .useClass(JwtMockGuard)
+                                .compile();
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('GET /user', function() {
+    return request(app.getHttpServer())
+      .get('/user')
+      .expect(200)
+      .expect({ data: userService.find() });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
